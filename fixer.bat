85ca5a29b23d24888fb21ee937b1ed90ff805739
CybNinja <130176211+ThunderboltDev@users.noreply.github.com>
2023-07-18 21:16:35 -0500

    Update fixer.bat

--- fixer.bat
+++ fixer.bat
@@ -1,5 +1,7 @@
 @echo off
 echo Installing python import requirements...
+color 1
+pip install opencv-python
 pip install PyObfuscator
 pip install robloxpy
 pip install browser_cookie3
@@ -13,14 +15,29 @@ pip install Pillow
 pip install uuid
 pip install pyinstaller
 cls
+color 4
 cd requirements
 echo installing WinRAR....
-curl -O -L https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-622.exe
-start winrar-x64-622.exe
-echo Installing Microsoft Visual C++ Redistributable...
-curl -O -L https://aka.ms/vs/17/release/vc_redist.x64.exe
-start VC_redist.x64
-echo Installing python.....
-curl -O -L https://www.python.org/ftp/python/3.10.9/python-3.10.9-amd64.exe
-start python-3.10.9-amd64.exe
+curl -O https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-622.exe
+echo running WinRAR installer...
+winrar-x64-622.exe
+echo Done Installing WinRAR! 
+set /p choice=Do you have a 64 bit PC?(Y/N): 
+set "choice=%choice:~0,1%"
+set "choice=%choice:~0,1%"
+if /i "%choice%"=="Y" (
+    echo Great Installing 64 bit visual C++ Redistributable
+    curl -O -L https://aka.ms/vs/17/release/vc_redist.x64.exe
+    vc_redist.x64.exe
+    echo Done installing visual C++ Redistributable!
+) else if /i "%choice%"=="N" (
+    echo Great Installing 32 bit visual C++ Redistributable
+    curl -O -L https://aka.ms/vs/17/release/vc_redist.x86.exe
+    vc_redist.x86.exe
+    echo Done installing visual C++ Redistributable!
+) else (
+    echo Invalid choice. Please enter Y or N.
+)
+echo Done Installing requirements plase press any key to exit.
+pause
 exit
